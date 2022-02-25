53b3befcedbbabf38ec5406466aab569dd0a07d8
vipul-rajput <vipulpr88@gmail.com>
2022-02-25 19:41:33 +0530

    minute changes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express =  require("express");
 const path = require("path");
 
 const app = express();
+
+
+app.use("/static", express.static(path.resolve(__dirname , "frontend" , "static")));
+
+
 app.get("/*", (req ,res) => {
     res.sendFile(path.resolve(__dirname ,"frontend", "index.html"));
 });
