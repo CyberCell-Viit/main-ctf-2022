a1b7faa2b20076ea7366363230b1afbcb7885eaf
vipul-rajput <vipulpr88@gmail.com>
2022-02-25 19:35:53 +0530

    bug fix

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const express =  require("express");
 const path = require("path");
 
 const app = express();
-var favicon = require('serve-favicon');
-
-app.use(favicon(__dirname + '/public/favicon.ico'));
-
-app.use("/static", express.static(path.resolve(__dirname , "frontend" , "static")));
-
-
 app.get("/*", (req ,res) => {
     res.sendFile(path.resolve(__dirname ,"frontend", "index.html"));
 });
